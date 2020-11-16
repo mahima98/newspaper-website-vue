4284dd0adfa964e2f64b807e3e5a97c2ff246de2
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-16 19:09:44 +0400

    Added Ads component in Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     <Menubar />
+    <Ads />
   </div>
 </template>
 
 <script>
 import Menubar from "../components/Menubar.vue";
+import Ads from "../components/Ads.vue";
 
 export default {
   components: {
     Menubar,
+    Ads,
   },
 };
 </script>
