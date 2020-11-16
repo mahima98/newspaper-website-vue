261f9f387aab0b69f7e6d80cfc8ef9a571cb1b05
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-16 19:08:15 +0400

    Created Ads.vue

---
+++ src/components/Ads.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-gray-200">
+    <div class="container mx-auto">
+      <div class="flex items-center justify-center">
+        <img class="py-8" src="../assets/images/ads.gif" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
