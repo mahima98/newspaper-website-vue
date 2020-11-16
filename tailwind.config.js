ea333efda16f02451069678f03ad935060b8f5de
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-16 17:08:17 +0400

    tailwind.config.js updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
   },
   purge: [],
   theme: {
-    extend: {},
+    fontWeight: {
+      light: 100,
+       thin: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+       semibold: 600,
+        bold: 700,
+       extrabold: 800,
+        black: 900,
+      }
+    
   },
   variants: {},
   plugins: [],
