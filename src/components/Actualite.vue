72db2a7e4fac800a3b095ca69af08b5667766100
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-17 15:56:14 +0400

    Actualite.vue updated with 2 components; box1 and box2

--- src/components/Actualite.vue
+++ src/components/Actualite.vue
@@ -1,6 +1,39 @@
 <template>
-  <div>
-    <div class="actuality-bar"></div>
-    <div class="actuality-components"></div>
+  <div class="Actualite-section container mx-auto">
+    <div class="actuality-bar py-2">
+      <div class="border-b-8 border-blue-800">
+        <div class="flex justify-left text-3xl font-sans font-light">
+          Actualite
+        </div>
+      </div>
+    </div>
+    <div class="actuality-components">
+      <div class="grid grid-cols-4">
+        <div class="first-col col-span-2">
+          <div class="space-y-2">
+            <ActualiteBox1 />
+            <ActualiteBox2 />
+            <ActualiteBox1 />
+          </div>
+        </div>
+        <div class="second-col"></div>
+        <div class="third-col"></div>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+import ActualiteBox1 from "../components/ActualiteBox1.vue";
+import ActualiteBox2 from "../components/ActualiteBox2.vue";
+
+export default {
+  components: {
+    ActualiteBox1,
+    ActualiteBox2,
+  },
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap");
+</style>
