d3c12b507f6908a8c0af9acafbc7b79099617f68
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-17 14:58:19 +0400

    Actualite component added in home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <Menubar />
     <Ads />
+    <Actualite />
   </div>
 </template>
 
 <script>
 import Menubar from "../components/Menubar.vue";
 import Ads from "../components/Ads.vue";
+import Actualite from "../components/Actualite.vue";
 
 export default {
   components: {
     Menubar,
     Ads,
+    Actualite,
   },
 };
 </script>
