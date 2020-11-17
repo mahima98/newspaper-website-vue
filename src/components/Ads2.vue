b93dcdd36ff60aa5bdf8f71cdcbe01fe396d83bf
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-17 21:50:05 +0400

    Ads.vue updated

--- src/components/Ads2.vue
+++ src/components/Ads2.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="Ads2">
-    <img class="object-cover w-full" src="../assets/images/image10.png" />
+    <img
+      class="object-cover h-full w-full"
+      src="../assets/images/image10.png"
+    />
   </div>
 </template>
 <script>
