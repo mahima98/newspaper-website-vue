a2ed62e4062917bf8b0ef898d9a698f15b6edaa0
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-17 16:37:14 +0400

    Actualite.vue updated with Ads2.vue and ActualiteBox3.vue

--- src/components/Actualite.vue
+++ src/components/Actualite.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="Actualite-section container mx-auto">
+  <div class="Actualite container mx-auto">
     <div class="actuality-bar py-2">
       <div class="border-b-8 border-blue-800">
         <div class="flex justify-left text-3xl font-sans font-light">
@@ -8,7 +8,7 @@
       </div>
     </div>
     <div class="actuality-components">
-      <div class="grid grid-cols-4">
+      <div class="grid grid-cols-4 space-x-4">
         <div class="first-col col-span-2">
           <div class="space-y-2">
             <ActualiteBox1 />
@@ -16,7 +16,12 @@
             <ActualiteBox1 />
           </div>
         </div>
-        <div class="second-col"></div>
+        <div class="second-col">
+          <Ads2 />
+          <div>
+            <ActualiteBox3 />
+          </div>
+        </div>
         <div class="third-col"></div>
       </div>
     </div>
@@ -25,11 +30,15 @@
 <script>
 import ActualiteBox1 from "../components/ActualiteBox1.vue";
 import ActualiteBox2 from "../components/ActualiteBox2.vue";
+import Ads2 from "../components/Ads2.vue";
+import ActualiteBox3 from "../components/ActualiteBox3.vue";
 
 export default {
   components: {
     ActualiteBox1,
     ActualiteBox2,
+    Ads2,
+    ActualiteBox3,
   },
 };
 </script>
