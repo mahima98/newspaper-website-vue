99626c9df4e80e3357f2bfc64cfd64e4588b654e
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-18 13:11:45 +0400

    Added Multimedia.vue

---
+++ src/components/Multimedia.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="Multimedia bg-gray-200">
+    <div class="container mx-auto">
+      <div class="multimedia-bar py-2">
+        <div class="border-b-4 border-pink-700">
+          <div class="flex justify-between items-end">
+            <div class="text-3xl font-sans font-light">
+              Multimedia
+            </div>
+            <div class="text-md px-2">
+              <a href="" class="hover:underline">Plus d'element multimedia »</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="multimedia-video-header">
+        <div class="flex">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-youtube"
+            >
+              <path
+                d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"
+              ></path>
+              <polygon
+                points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"
+              ></polygon>
+            </svg>
+          </div>
+          <div>Videos</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
