96b4051aa0faeffbd5963b82bede921c2f0934b1
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-19 18:54:48 +0400

    Multimedia.vue updated

--- src/components/Multimedia.vue
+++ src/components/Multimedia.vue
@@ -14,12 +14,12 @@
         </div>
       </div>
       <div class="multimedia-video-header">
-        <div class="flex">
-          <div>
+        <div class="flex space-x-2">
+          <div class="flex justify-center items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
+              width="20"
+              height="20"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
@@ -36,12 +36,121 @@
               ></polygon>
             </svg>
           </div>
-          <div>Videos</div>
+          <div class="flex justify-center items-center">
+            <div>Videos »</div>
+          </div>
+        </div>
+      </div>
+      <div class="multimedia-video-section py-2">
+        <div class="grid grid-cols-4 gap-4">
+          <MultimediaBox1
+            v-for="(description, index) in firstdatas"
+            :myvaluesmultimedia="description"
+            :key="index"
+          />
+        </div>
+      </div>
+      <div class="grid grid-cols-2 gap-4">
+        <div class="multimedia-a-ecouter">
+          <div class="multimedia-a-ecouter-header">
+            <div class="flex space-x-2">
+              <div class="flex justify-center items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-mic"
+                >
+                  <path
+                    d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"
+                  ></path>
+                  <path d="M19 10v2a7 7 0 0 1-14 0v-2"></path>
+                  <line x1="12" y1="19" x2="12" y2="23"></line>
+                  <line x1="8" y1="23" x2="16" y2="23"></line>
+                </svg>
+              </div>
+              <div class="flex justify-center items-center">
+                <div>A ecouter »</div>
+              </div>
+            </div>
+          </div>
+          <div class="multimedia-a-ecouter-section py-2">
+            <div class="grid grid-cols-2 gap-4">
+              <MultimediaBox2
+                v-for="(description, index) in secondatas"
+                :myvaluesmultimedia="description"
+                :key="index"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="multimedia-diaporamas">
+          <div class="multimedia-diaporamas-header">
+            <div class="flex space-x-2">
+              <div class="flex justify-center items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="feather feather-camera"
+                >
+                  <path
+                    d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"
+                  ></path>
+                  <circle cx="12" cy="13" r="4"></circle>
+                </svg>
+              </div>
+              <div class="flex justify-center items-center">
+                <div>Diaporamas »</div>
+              </div>
+            </div>
+          </div>
+          <div class="multimedia-video-section py-2">
+            <div class="grid grid-cols-2 gap-4">
+              <MultimediaBox3
+                v-for="(description, index) in thirdatas"
+                :myvaluesmultimedia="description"
+                :key="index"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-export default {};
+import MultimediaBox1 from "../components/MultimediaBox1.vue";
+import MultimediaBox2 from "../components/MultimediaBox2.vue";
+import MultimediaBox3 from "../components/MultimediaBox3.vue";
+
+import cards from "../data.js";
+
+export default {
+  components: {
+    MultimediaBox1,
+    MultimediaBox2,
+    MultimediaBox3,
+  },
+  data: () => {
+    return {
+      //   subtitle: false,
+      firstdatas: cards.slice(0, 4),
+      secondatas: cards.slice(4, 6),
+      thirdatas: cards.slice(6, 8),
+    };
+  },
+};
 </script>
