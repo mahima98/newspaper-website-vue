ebb84913cd1550ca254e99e3717b628312e8cfe0
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-23 15:57:07 +0400

    Article.vue updated

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="Article">
     <Header />
+    <ArticleBox />
   </div>
 </template>
 <script>
 import Header from "../components/Header.vue";
+import ArticleBox from "../components/ArticleBox.vue";
 
 export default {
   components: {
     Header,
+    ArticleBox,
   },
 };
 </script>
