d067fdec2233accc2ca3f3c660252b882cb699eb
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-23 15:11:09 +0400

    Header added

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Menu />
+    <div class="bg-gray-200">
+      <div class="container mx-auto">
+        <div class="flex items-center justify-center">
+          <img class="h-64 py-8" src="../assets/images/ads.gif" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Menu from "../components/Menu.vue";
+export default {
+  components: {
+    Menu,
+  },
+};
+</script>
