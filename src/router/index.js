537e106fea29cd97d5e2a0fa25c069d59b468b9c
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-23 16:05:08 +0400

    index.js updated

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,16 @@ const routes = [
     name: "Home",
     component: Home
   },
+    {
+    path: "/home",
+    name: "Home",
+    // route level code-splitting
+    // this generates a separate chunk (Home.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "Home" */ "../views/Home.vue")
+    
+  },
   {
     path: "/about",
     name: "About",
@@ -18,15 +28,16 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+    
   },
     {
     path: "/article",
     name: "Article",
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (article.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/Article.vue")
+      import(/* webpackChunkName: "article" */ "../views/Article.vue")
   }
 ];
 
