b6faca00c45319531eb63889785c10bfa10ba08b
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-23 15:56:54 +0400

    added ArticleBox.vue

---
+++ src/components/ArticleBox.vue
@@ -0,0 +1,257 @@
+<template>
+  <div class="ArticleBox">
+    <div class="container mx-auto">
+      <div class="actuality-bar py-2">
+        <div class="border-b-4 border-red-700">
+          <div class="flex justify-left text-3xl font-sans font-light">
+            Elections villageoises: des signes qui ne trompent pas
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-between my-4 space-x-4">
+        <div class="firstCol">
+          <div class="flex">
+            <div class="h-16 w-16">
+              <img
+                class="object-cover h-16 w-16 rounded-full"
+                src="../assets/images/image18.png"
+              />
+            </div>
+            <div class="time px-4 py-2">
+              <div class="text-xss text-gray-300 ">
+                <div class="flex items-center space-x-1">
+                  <div>
+                    <svg
+                      width="11"
+                      height="11"
+                      viewBox="0 0 8 8"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M6.33333 1.33331H1.66667C1.29848 1.33331 1 1.63179 1 1.99998V6.66665C1 7.03484 1.29848 7.33331 1.66667 7.33331H6.33333C6.70152 7.33331 7 7.03484 7 6.66665V1.99998C7 1.63179 6.70152 1.33331 6.33333 1.33331Z"
+                        stroke="gray"
+                        stroke-width="0.666667"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M5.33325 0.666687V2.00002"
+                        stroke="gray"
+                        stroke-width="0.666667"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M2.66675 0.666687V2.00002"
+                        stroke="gray"
+                        stroke-width="0.666667"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M1 3.33331H7"
+                        stroke="gray"
+                        stroke-width="0.666667"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </div>
+                  <div class="">23 NOV 2020</div>
+                </div>
+                <div class="flex items-center space-x-1">
+                  <div class="">
+                    <svg
+                      width="11"
+                      height="11"
+                      viewBox="0 0 8 8"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4.00008 7.33335C5.84103 7.33335 7.33342 5.84097 7.33342 4.00002C7.33342 2.15907 5.84103 0.666687 4.00008 0.666687C2.15913 0.666687 0.666748 2.15907 0.666748 4.00002C0.666748 5.84097 2.15913 7.33335 4.00008 7.33335Z"
+                        stroke="gray"
+                        stroke-width="0.666667"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M4 2V4L5.33333 4.66667"
+                        stroke="gray"
+                        stroke-width="0.666667"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </div>
+                  <div>11:00</div>
+                </div>
+                <div class="flex items-center space-x-1">
+                  <div>
+                    <svg
+                      width="11"
+                      height="11"
+                      viewBox="0 0 9 9"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clip-path="url(#clip0)">
+                        <path
+                          d="M7.75 6.5C7.75 6.69891 7.67098 6.88968 7.53033 7.03033C7.38968 7.17098 7.19891 7.25 7 7.25H2.5L1 8.75V2.75C1 2.55109 1.07902 2.36032 1.21967 2.21967C1.36032 2.07902 1.55109 2 1.75 2H7C7.19891 2 7.38968 2.07902 7.53033 2.21967C7.67098 2.36032 7.75 2.55109 7.75 2.75V6.5Z"
+                          stroke="gray"
+                          stroke-width="0.666667"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0">
+                          <rect width="9" height="9" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </div>
+                  <div>0</div>
+                </div>
+              </div>
+              <div class="ads space-y-2 py-2">
+                <Ads2 />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="secondCol">
+          <div>
+            <img class="object-cover" src="../assets/images/elections1.jpg" />
+          </div>
+          <div class="text-md text-left max-w-2xl my-4 space-y-4">
+            <div>
+              Il n’y a eu ni soleil, ni clé encore moins de cœur ou de coq.
+              Pourtant, il y a eu des groupes proches des partis traditionnels
+              et ils sont dirigés, voire parrainées, par des ministres. S’ils
+              n’utilisent pas les symboles des partis politiques, ceux qui ont
+              sillonné les différents villages hier ont dû s’apercevoir que les
+              candidats portaient des couleurs associées aux partis – comme
+              l’orange, le rouge, le bleu et il y a eu même le rouge-mauve-bleu
+              associé au symbole ciseaux.
+            </div>
+            <div>
+              Cependant, les groupes soutenus par le gouvernement s’affichaient
+              ouvertement. À Lalmatie par exemple, le groupe Lallmatie Revival
+              Party avait dressé une photo de Pravind Jugnauth sous un
+              chapiteau. Ainsi, comme beaucoup de proches du Mouvement
+              socialiste militant (MSM) ne peuvent pas utiliser le nom de ce
+              parti, ils ont trouvé une astuce pour montrer leur appartenance au
+              gouvernement.
+            </div>
+            <div>
+              Par exemple, il y a eu le Mouvement Socialiste Volontaire de
+              Goodlands ou encore le Mouvement Socialiste de Saint-Pierre.
+              Est-ce une coïncidence que ces groupes ont comme symbole le
+              parasol ? On associe bien le parasol avec le soleil du MSM.
+              D’ailleurs, Alan Ganoo lui-même a ouvertement donné des consignes
+              pour voter le Mouvement Social de Chemin-Grenier qui avait comme
+              symbole… un parasol.
+            </div>
+          </div>
+        </div>
+        <div class="thirdCol">
+          <div class="space-y-2 py-2">
+            <div class="text-center text-gray-500">
+              <a href="">Partager cette carricature</a>
+            </div>
+            <div class="grid grid-cols-3">
+              <button
+                class="flex justify-center items-center space-x-1 py-4 px-8 text-white bg-blue-500"
+              >
+                <div>
+                  <svg
+                    width="25"
+                    height="21"
+                    viewBox="0 0 25 21"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M18.75 1.75H15.625C14.2437 1.75 12.9189 2.21094 11.9421 3.03141C10.9654 3.85188 10.4167 4.96468 10.4167 6.125V8.75H7.29166V12.25H10.4167V19.25H14.5833V12.25H17.7083L18.75 8.75H14.5833V6.125C14.5833 5.89294 14.6931 5.67038 14.8884 5.50628C15.0838 5.34219 15.3487 5.25 15.625 5.25H18.75V1.75Z"
+                      stroke="white"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+              </button>
+              <button
+                class="flex justify-center items-center space-x-1 py-4 px-8 text-black bg-blue-600"
+              >
+                <div class="text-white">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="feather feather-twitter"
+                  >
+                    <path
+                      d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
+                    ></path>
+                  </svg>
+                </div>
+              </button>
+              <button
+                class="flex justify-center items-center space-x-1 py-4 px-4 text-black bg-black"
+              >
+                <div>
+                  <svg
+                    width="21"
+                    height="18"
+                    viewBox="0 0 21 18"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M3.5 3H17.5C18.4625 3 19.25 3.675 19.25 4.5V13.5C19.25 14.325 18.4625 15 17.5 15H3.5C2.5375 15 1.75 14.325 1.75 13.5V4.5C1.75 3.675 2.5375 3 3.5 3Z"
+                      stroke="white"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M19.25 4.5L10.5 9.75L1.75 4.5"
+                      stroke="white"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+              </button>
+            </div>
+            <div>
+              <img
+                class="object-cover w-full rounded-full"
+                src="../assets/images/car.png"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import Ads2 from "../components/Ads2.vue";
+
+export default {
+  components: {
+    Ads2,
+  },
+};
+</script>
