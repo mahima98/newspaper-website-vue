299142633d56b6b40b0ede336ea2aa5378b51346
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-24 18:30:14 +0400

    sample example page1 page2

---
+++ src/views/Page1.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="page1">
+    <h1>{{ msg }}</h1>
+    <router-link :to="{ name: 'Page2', params: { id: 1234 } }"
+      >Navigate to Page2</router-link
+    >
+  </div>
+</template>
+s
---
+++ src/views/Page2.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="page2">
+    <h1>{{ msg }}, your id is {{ id }}</h1>
+    <a
+      style="cursor: pointer; text-decoration: underline"
+      v-on:click="navigate()"
+      >Navigate to Page1</a
+    >
+  </div>
+</template>
+
+<script>
+import router from "../router";
+
+export default {
+  name: "Page2",
+  data() {
+    return {
+      id: 0,
+      msg: "Hey Nic Raboy",
+    };
+  },
+  created() {
+    this.id = this.$route.params.id;
+  },
+  methods: {
+    navigate() {
+      router.go(-1);
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
