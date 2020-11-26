915e2921a0b2260a5a52a8d55a7c0ad92a8a7703
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-26 16:09:08 +0400

    ads2.vue added

--- src/components/Ads2.vue
+++ src/components/Ads2.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="Ads2">
-    <img class="object-cover h-64 w-full" src="../assets/images/image10.png" />
-  </div>
+  <div class="Ads2"></div>
 </template>
 <script>
 export default {};
