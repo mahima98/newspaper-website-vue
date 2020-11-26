ae154fd63a6e10592728f6d9b7024cad6b2167cd
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-26 20:03:08 +0400

    main.js updated

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import '@/assets/tailwind.css';
+ import VueResource from 'vue-resource'
 
-
+Vue.use( VueResource)
 Vue.config.productionTip = false;
 
 new Vue({
