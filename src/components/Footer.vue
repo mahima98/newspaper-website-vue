0df8a1082c1e23c6c9991f4c42e3a42f050c8dee
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-30 11:55:35 +0400

    images added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,57 +1,89 @@
 <template>
-  <div class="footer bg-blue-800">
-    <div class="container mx-auto">
-      <div class="grid grid-cols-2">
-        <div class="description py-4 my-4">
-          <div class="grid grid-cols-3">
-            <div class="col-span-2 text-left text-white space-y-4 pr-12">
-              <div>Veuillez noter</div>
-              <div class="text-sm">
-                Depuis le 23 mars 2020, nous avons fermé l'application de
-                l'express
-              </div>
-              <div class="text-sm">
-                Nous basculons tous les utilisateurs vers Kiosk LSL, pour plus
-                d'informations, contactez-nous en cliquant sur ce bouton:
-              </div>
-              <div class="flex items-center justify-center bg-orange-600">
-                <div class="text-sm p-2 ">
-                  <svg
-                    width="20"
-                    height="20"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M4 4H20C21.1 4 22 4.9 22 6V18C22 19.1 21.1 20 20 20H4C2.9 20 2 19.1 2 18V6C2 4.9 2.9 4 4 4Z"
-                      stroke="white"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                    <path
-                      d="M22 6L12 13L2 6"
-                      stroke="white"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
+  <div class="footer">
+    <div class="bg-blue-800">
+      <div class="container mx-auto">
+        <div class="grid grid-cols-2">
+          <div class="description py-4 my-4">
+            <div class="grid grid-cols-3">
+              <div class="col-span-2 text-left text-white space-y-4 pr-12">
+                <div>Veuillez noter</div>
+                <div class="text-sm">
+                  Depuis le 23 mars 2020, nous avons fermé l'application de
+                  l'express
                 </div>
-                <div class="text-xs">CONTACTEZ NOTRE ÉQUIPE D'ASSISTANCE</div>
+                <div class="text-sm">
+                  Nous basculons tous les utilisateurs vers Kiosk LSL, pour plus
+                  d'informations, contactez-nous en cliquant sur ce bouton:
+                </div>
+                <div class="flex items-center justify-center bg-orange-600">
+                  <div class="text-sm p-2 ">
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4 4H20C21.1 4 22 4.9 22 6V18C22 19.1 21.1 20 20 20H4C2.9 20 2 19.1 2 18V6C2 4.9 2.9 4 4 4Z"
+                        stroke="white"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                      <path
+                        d="M22 6L12 13L2 6"
+                        stroke="white"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </div>
+                  <div class="text-xs">CONTACTEZ NOTRE ÉQUIPE D'ASSISTANCE</div>
+                </div>
+                <div class="flex space-x-2">
+                  <div><img src="../assets/images/image21.png" alt="" /></div>
+                  <div><img src="../assets/images/image22.png" alt="" /></div>
+                </div>
               </div>
-              <div class="flex space-x-2">
-                <div><img src="../assets/images/image21.png" alt="" /></div>
-                <div><img src="../assets/images/image22.png" alt="" /></div>
+              <div class="image">
+                <img src="../assets/images/image23.png" alt="" />
               </div>
-            </div>
-            <div class="image">
-              <img src="../assets/images/image23.png" alt="" />
             </div>
           </div>
+          <div></div>
         </div>
-        <div></div>
+      </div>
+    </div>
+    <div class="bg-black">
+      <div class="container mx-auto">
+        <div class="parti-sociaux grid grid-cols-4 gap-4">
+          <div class="les-partenaire col-span-3">
+            <div class="text-white">
+              Les autres sites du groupe La Sentinelle et partenaires
+            </div>
+            <div class="flex">
+              <img src="../assets/images/image24.png" alt="" /><img
+                src="../assets/images/image25.png"
+                alt=""
+              /><img src="" alt="" /><img
+                src="../assets/images/image26.png"
+                alt=""
+              /><img src="../assets/images/image27.png" alt="" /><img
+                src=""
+                alt=""
+              /><img src="../assets/images/image28.png" alt="" /><img
+                src="../assets/images/image29.png"
+                alt=""
+              /><img src="../assets/images/image30.png" alt="" />
+              <img src="../assets/images/image31.png" alt="" />
+              <img src="../assets/images/image32.png" alt="" />
+            </div>
+          </div>
+          <div class="Réseaux sociaux"></div>
+        </div>
+        <div class="plan-du-site"></div>
       </div>
     </div>
   </div>
--- src/components/Idee.vue
+++ src/components/Idee.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="idee">
-    <div class="container mx-auto">
+    <div class="container mx-auto py-4">
       <div class="idee-bar py-2">
         <div class="border-b-4 border-pink-700">
           <div class="flex justify-between items-end">
