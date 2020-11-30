1b82ce85b85117e0f93f9435d0f6db93a2cf9090
Mahima Ramgolam <mahima@ramgolam.com>
2020-11-30 11:40:39 +0400

    Added footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="footer bg-blue-800">
+    <div class="container mx-auto">
+      <div class="grid grid-cols-2">
+        <div class="description py-4 my-4">
+          <div class="grid grid-cols-3">
+            <div class="col-span-2 text-left text-white space-y-4 pr-12">
+              <div>Veuillez noter</div>
+              <div class="text-sm">
+                Depuis le 23 mars 2020, nous avons fermé l'application de
+                l'express
+              </div>
+              <div class="text-sm">
+                Nous basculons tous les utilisateurs vers Kiosk LSL, pour plus
+                d'informations, contactez-nous en cliquant sur ce bouton:
+              </div>
+              <div class="flex items-center justify-center bg-orange-600">
+                <div class="text-sm p-2 ">
+                  <svg
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4 4H20C21.1 4 22 4.9 22 6V18C22 19.1 21.1 20 20 20H4C2.9 20 2 19.1 2 18V6C2 4.9 2.9 4 4 4Z"
+                      stroke="white"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M22 6L12 13L2 6"
+                      stroke="white"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </div>
+                <div class="text-xs">CONTACTEZ NOTRE ÉQUIPE D'ASSISTANCE</div>
+              </div>
+              <div class="flex space-x-2">
+                <div><img src="../assets/images/image21.png" alt="" /></div>
+                <div><img src="../assets/images/image22.png" alt="" /></div>
+              </div>
+            </div>
+            <div class="image">
+              <img src="../assets/images/image23.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  </div>
+</template>
